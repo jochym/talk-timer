6144402b7d346e0a8674156231d205023abbb7c8
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2018-03-11 18:03:52 +0100

    Port to gst-1.0 and gtk3

--- timer.py
+++ timer.py
@@ -6,12 +6,11 @@
 # (L) 2010 Pawel T. Jochym
 # The code below is licensed under GPLv3 or later
 
-import pygtk
-pygtk.require('2.0')
-import gtk, gobject
-import pygst
-pygst.require("0.10")
-import gst
+import gi
+gi.require_version('Gst', '1.0')
+gi.require_version('Gtk', '3.0')
+from gi.repository import GObject, Gst, Gtk
+
 import math
 import cairo
 import sys
@@ -37,13 +36,16 @@ oneMinute=60
 #Specify your alert file bellow 
 #It can be any audio supported by gstreamer
 file = "/usr/share/sounds/gnome/default/alerts/glass.ogg"
+file = "/usr/share/sounds/gnome/default/alerts/drip.ogg"
 
 #Create a player
+
+Gst.init(None)
 
 class Player:
     def __init__(self, file):
         #Element playbin automatic plays any file
-        self.player = gst.element_factory_make("playbin", "player")
+        self.player = Gst.ElementFactory.make("playbin", "player")
         #Set the uri to the file
         self.player.set_property("uri", "file://" + file)
 
@@ -54,16 +56,16 @@ class Player:
 
 
     def run(self):
-        self.player.set_state(gst.STATE_PLAYING)
+        self.player.set_state(Gst.State.PLAYING)
 
     def on_message(self, bus, message):
         t = message.type
-        if t == gst.MESSAGE_EOS:
+        if t == Gst.MessageType.EOS:
             #file ended, stop
-            self.player.set_state(gst.STATE_NULL)
-        elif t == gst.MESSAGE_ERROR:
+            self.player.set_state(Gst.State.NULL)
+        elif t == Gst.MessageType.ERROR:
             #Error ocurred, print and stop
-            self.player.set_state(gst.STATE_NULL)
+            self.player.set_state(Gst.State.NULL)
             err, debug = message.parse_error()
             print "Error: %s" % err, debug
 
@@ -73,17 +75,20 @@ class PresentationTimer:
 
 #TODO The GUI setup should be moved to the glade definition file.
 
-        window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        window = Gtk.Window(Gtk.WindowType.TOPLEVEL)
         window.set_title("Presentation Timer")
-        window.connect("destroy", lambda w: gtk.main_quit())
-        self.area = gtk.DrawingArea()
-        self.area.set_size_request(480, 480)
-        vbox=gtk.VBox()
-        vbox.pack_start(self.area,True,True)
-        hbox=gtk.HBox(True)
-        vbox.pack_end(hbox,False,False)
+        window.connect("destroy", lambda w: Gtk.main_quit())
+        self.area = Gtk.DrawingArea()
+        self.width=480
+        self.height=480
+        self.area.set_size_request(self.width, self.height )
+        vbox=Gtk.VBox()
+        vbox.pack_start(self.area,True,True,0)
+        hbox=Gtk.HBox(True)
+        vbox.pack_end(hbox,False,False,0)
         window.add(vbox)
-        self.area.connect("expose-event", self.expose)
+        self.area.connect("draw", self.expose)
+        self.area.connect("size-allocate", self.on_size_allocate)
         self.area.show()
 
         self.timer=None
@@ -94,10 +99,12 @@ class PresentationTimer:
         self.now = self.Tstart
         self.running = False
 
-        adj1 = gtk.Adjustment(15, 1, 120, 1, 5)
-        spinner1 = gtk.SpinButton(adj1, 0, 0)
-        adj2 = gtk.Adjustment(5, 1, 45, 1, 5)
-        spinner2 = gtk.SpinButton(adj2, 0, 0)
+        adj1 = Gtk.Adjustment(15, 1, 120, 1, 5)
+        spinner1 = Gtk.SpinButton()
+	spinner1.set_adjustment(adj1)
+        adj2 = Gtk.Adjustment(5, 1, 45, 1, 5)
+        spinner2 = Gtk.SpinButton()
+	spinner2.set_adjustment(adj2)
 
         adj1.connect("value_changed", self.setupChanged, spinner1, spinner2)
         adj2.connect("value_changed", self.setupChanged, spinner1, spinner2)
@@ -107,34 +114,34 @@ class PresentationTimer:
         self.overtime = False
         self.ringed = False
         
-        button = gtk.Button("Start")
+        button = Gtk.Button("Start")
         button.connect("clicked", self.startClock, spinner1, spinner2)
-        hbox.pack_start(button, False, False)
+        hbox.pack_start(button, False, False, 0)
 
 #TODO No pausing for now
-#        button = gtk.Button("Pause")
+#        button = Gtk.Button("Pause")
 #        button.connect("clicked", self.pauseClock, spinner1, spinner2)
 #        hbox.pack_start(button, False, False)
 
-        button = gtk.Button("Stop")
+        button = Gtk.Button("Stop")
         button.connect("clicked", self.stopClock, "Stop button")
-        hbox.pack_start(button, False, False)
+        hbox.pack_start(button, False, False, 0)
 
-        hbox1=gtk.HBox(False)
-        hbox.pack_end(hbox1, False, False)
+        hbox1=Gtk.HBox(False)
+        hbox.pack_end(hbox1, False, False, 0)
 
-        label=gtk.Label("Talk:")
-        hbox1.pack_start(label,False,False)
-        hbox1.pack_start(spinner1,False,False)
+        label=Gtk.Label("Talk:")
+        hbox1.pack_start(label,False,False, 0)
+        hbox1.pack_start(spinner1,False,False, 0)
         
-        label=gtk.Label("Discussion:")
-        hbox1.pack_start(label,False,False)
-        hbox1.pack_start(spinner2,False,False)
+        label=Gtk.Label("Discussion:")
+        hbox1.pack_start(label,False,False,0)
+        hbox1.pack_start(spinner2,False,False,0)
 
         window.show_all()
         
         # Start the display update clock (ticks/second)
-        self.clock=gobject.timeout_add(oneSecond/ticks, self.updateClock)
+        self.clock=GObject.timeout_add(oneSecond/ticks, self.updateClock)
 
 
     def setupClock(self, spinTalk, spinDisc):
@@ -150,7 +157,7 @@ class PresentationTimer:
     def startClock(self, wdg, spinTalk, spinDisc):
         self.setupClock(spinTalk, spinDisc)
         if self.Talert :
-            gobject.source_remove(self.Talert)
+            GObject.source_remove(self.Talert)
             self.Talert=None
         self.Tstart = time.time()
         self.now = self.Tstart
@@ -162,7 +169,7 @@ class PresentationTimer:
     def stopClock(self,wdg,data=None):
         self.overtime=False
         if self.Talert :
-            gobject.source_remove(self.Talert)
+            GObject.source_remove(self.Talert)
             self.Talert=None
         self.running = False
         self.ringed = False
@@ -173,7 +180,7 @@ class PresentationTimer:
 #TODO Pausing not implemented yet
     def pauseClock(self,wdg, spinTalk, spinDisc):
         if self.Talert :
-            gobject.source_remove(self.Talert)
+            GObject.source_remove(self.Talert)
             self.Talert=None
         self.running = False
         self.pause = True
@@ -186,6 +193,11 @@ class PresentationTimer:
             not self.ringed) :
                 self.ringed = True
                 self.alert()
+        # The time has been added after overtime => reset overtime
+        if (self.overtime and 
+            (time.time() < (self.Tstart + self.total))):
+            self.overtime = False
+            self.running = True
         # We are first time over total length
         if (self.running and 
             (time.time() > (self.Tstart + self.total)) and 
@@ -194,13 +206,17 @@ class PresentationTimer:
                 self.running = False
                 self.alert()
                 if not self.Talert :
-                    self.Talert=gobject.timeout_add(oneSecond*alerts, self.alert)
+                    self.Talert=GObject.timeout_add(oneSecond*alerts, self.alert)
         self.area.queue_draw()
         return True
 
+    def on_size_allocate(self, _unused, allocation):
+        self.width = allocation.width
+        self.height = allocation.height
+
     def expose(self, area, event):
-        self.context = area.window.cairo_create()
-        self.draw_clock(event.area.width/2, event.area.height/2)
+        self.context = area.get_property('window').cairo_create()
+        self.draw_clock(self.width/2, self.height/2)
         return False
 
     def alert(self):
@@ -222,6 +238,7 @@ class PresentationTimer:
         if c<0 :
             c=int(self.total-dT)
         t=dT/self.total
+        tleft=self.total-c
         a=-math.pi/2
         
         r=min(x,y)-10
@@ -248,21 +265,26 @@ class PresentationTimer:
         cx.line_to(x,y)
         cx.set_source_rgb(0,0,0)
         cx.stroke()
-        cx.select_font_face("Courier", 
+        cx.select_font_face("Monospace", 
             cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
         cx.set_font_size(r/3)
         cx.move_to(x-r/2,y+r/2)
         cx.set_source_rgb(1, 0.3, 0.3)
-        cx.show_text("%2d:%02d" % (c/60, (c)%(60)))
-        if self.overtime :
-            cx.select_font_face("Arial", 
-                cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
-            cx.set_font_size(r/4)
-            cx.move_to(x-r/1.5,y-r/5)
-            cx.set_source_rgb(1, 0, 0)
-            cx.show_text("OVERTIME!")
-
-        cx.select_font_face("Courier", 
+        
+        if self.overtime:
+            tleft=int(time.time())-self.now
+            if tleft > 0 : cx.show_text("%2d:%02d" % (tleft/60, (tleft)%(60)))
+            if (int(time.time())%2)==0 :
+                cx.select_font_face("Arial", 
+                    cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
+                cx.set_font_size(r/4)
+                cx.move_to(x-r/1.5,y-r/5)
+                cx.set_source_rgb(1, 0, 0)
+                cx.show_text("OVERTIME!")
+        else :
+            cx.show_text("%2d:%02d" % (tleft/60, (tleft)%(60)))
+            
+        cx.select_font_face("Monospace", 
             cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
         cx.set_font_size(r/8)
         cx.move_to(0,r/10)
@@ -273,10 +295,14 @@ class PresentationTimer:
         return
 
 def main():
-    gtk.main()
+    Gtk.main()
     return 0
 
 if __name__ == "__main__":
+    import sys
+    
+    if len(sys.argv)>1 :
+        file=sys.argv[1].strip()
     PresentationTimer()
     main()
 
